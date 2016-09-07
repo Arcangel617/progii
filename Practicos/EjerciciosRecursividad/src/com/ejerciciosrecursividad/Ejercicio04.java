213a5ca06e9fbd16625d1a8f408389010de4d335
Arcangel Andres <artigue_arcangel@hotmail.com>
2016-09-07 10:08:05 -0300

    Ejercicios sin resolver

---
+++ Practicos/EjerciciosRecursividad/src/com/ejerciciosrecursividad/Ejercicio04.java
@@ -0,0 +1,16 @@
+package com.ejerciciosrecursividad;
+
+/**
+ * The fibonacci sequence is a famous bit of mathematics, and it happens to have a recursive definition.
+ * The first two values in the sequence are 0 and 1 (essentially 2 base cases). Each subsequent value is the
+ * sum of the previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21 and so on. Define a
+ * recursive fibonacci(n) method that returns the nth fibonacci number, with n=0 representing the start of the
+ * sequence.
+ * @author Artigue Arcangel Andres
+ * @version 030916
+ */
+public class Ejercicio04 {
+    public static void main(String [] args){
+        System.out.println("Ejercicio04");
+    }
+}
---
+++ Practicos/EjerciciosRecursividad/src/com/ejerciciosrecursividad/Ejercicio05.java
@@ -0,0 +1,13 @@
+package com.ejerciciosrecursividad;
+
+/**
+ * We have a number of bunnies and each bunny has two big floppy ears. We want to compute the total
+ * number of ears across all the bunnies recursively (without loops or multiplication).
+ * @author Artigue Arcangel Andres
+ * @version 030916
+ */
+public class Ejercicio05 {
+    public static void main(){
+
+    }
+}
