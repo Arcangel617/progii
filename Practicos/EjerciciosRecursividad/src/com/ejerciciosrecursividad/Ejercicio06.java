abc067b54769cb6c2546ffc6d2284f3462aed005
Arcangel Andres <artigue_arcangel@hotmail.com>
2016-09-11 18:59:52 -0300

    Se resuelve el ejercicio 06

---
+++ Practicos/EjerciciosRecursividad/src/com/ejerciciosrecursividad/Ejercicio06.java
@@ -0,0 +1,29 @@
+package com.ejerciciosrecursividad;
+
+/**
+ * We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies (1, 3, ..) have the normal 2 ears.
+ * The even bunnies (2, 4, ..) we'll say have 3 ears, because they each have a raised foot. Recursively return
+ * the number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+ * @author Artigue Arcangel Andres
+ * @version 110916
+ */
+public class Ejercicio06 {
+    public static void main(String [] args){
+        System.out.println(bunnyEars2(0));
+        System.out.println(bunnyEars2(1));
+        System.out.println(bunnyEars2(2));
+        System.out.println(bunnyEars2(3));
+        System.out.println(bunnyEars2(4));
+
+    }
+
+    public static int bunnyEars2(int n){
+        if (n == 0) {
+            return 0;
+        } else if (n % 2 == 0){
+            return bunnyEars2(n-1) + 3;
+        } else {
+            return bunnyEars2(n-1) + 2;
+        }
+    }
+}
